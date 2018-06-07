b9091c7090bcdd4fdc1c15dc9b803e24f956967a
Mitchell Combs <mitch.combs.92@gmail.com>
2018-06-07 11:02:03 -0400

    Initial Commit

--- CSharptoSqlLibrary/Program.cs
+++ CSharptoSqlLibrary/Program.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SqlLibrary;
 
 namespace CSharptoSqlLibrary {
 	class Program {
 		static void Main(string[] args) {
+
+			UsersController UserCtrl = new SqlLibrary.UsersController();
 		}
 	}
 }
---
+++ SqlLibrary/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlLibrary {
+	public class UsersController {
+
+		public UsersController () {
+
+		}
+
+		public IEnumerable<User> List() {
+			return new List<User>();
+		}
+
+		public User Get(int id) {
+			return null;
+		}
+
+		public bool Create(User user) {
+			return false;
+		}
+
+		public bool Change(User user) {
+			return false;
+		}
+
+		public bool Remove(User user) {
+			return false;
+		}
+	}
+}
